eef32041767b39ad7319c49274a1644b635818ea
Brenon Greigoletto <113072662+BrenonGrigoletto@users.noreply.github.com>
2024-03-29 18:41:29 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Programação WEB</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <h1>Programação WEB</h1> 
+  <h4>Menu</h4>
+  <div> <button type="button" onclick="location.href='lista2.php'">Alunos</button> <br>
+  </div> 
+
+  <footer>
+        <h5> ILP508A - Eletiva - Programação WEB</h5>
+        <h5> Profª: Vanessa Dos Anjos Borges</h5>
+        <h5> Fatec - Presidente Prudente</h5>
+</footer>
+</body>
+</html>
