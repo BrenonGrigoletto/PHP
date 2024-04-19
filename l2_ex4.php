3d8152973bc8e33c845b6d8c456a67c8778e6b30
Brenon Greigoletto <113072662+BrenonGrigoletto@users.noreply.github.com>
2024-04-19 00:10:04 -0300

    Update l2_ex4.php

--- l2_ex4.php
+++ l2_ex4.php
@@ -23,7 +23,7 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
-    <a href="lista2.php">Voltar</a>
+    <a href="lista2.php" class="btn btn-secondary mt-3">Voltar</a>
     <div class="container">
         <h1>Tabuada</h1>
         <form method="GET">
