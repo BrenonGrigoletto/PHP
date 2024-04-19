96fab649d8060e8a2ab1b88a3d6850d1386095a5
Brenon Greigoletto <113072662+BrenonGrigoletto@users.noreply.github.com>
2024-04-19 00:09:13 -0300

    Update l2_ex3.php

--- l2_ex3.php
+++ l2_ex3.php
@@ -25,7 +25,7 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <a href="lista2.php">Voltar</a>
+    <a href="lista2.php" class="btn btn-secondary mt-3">Voltar</a>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercicio 3</title>
